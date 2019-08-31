8197323915dbfd0c28212ea0c4af66925a54ab32
Gavin Quirk <gavinquirk@gmail.com>
2019-08-31 16:53:25 -0700

    header content and styles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,27 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+  <div class="row">
+    <div class="col-3">
+      <img src="https://www.rawshorts.com/freeicons/wp-content/uploads/2017/01/media-pict-camera.png" class="rounded-circle"
+      style="width: 100%"
+      alt="Logo">
     </div>
+    <div class="col-9 pt-5">
+      <div>
+        <h1>Pix</h1>
+      </div>
+      <div class="d-flex">
+        <div class="pr-5"><strong>153</strong> Posts</div>
+        <div class="pr-5"><strong>23K</strong> Followers</div>
+        <div class="pr-5"><strong>212</strong> Following</div>
+      </div>
+      <div class="pt-4 font-weight-bold">
+        Pix.org
+      </div>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati distinctio ipsum placeat tempora consequatur deleniti blanditiis voluptates natus pariatur veritatis.</div>
+      <div><a href="#"></a>www.pix.org</div>
+    </div>
+  </div>
 </div>
 @endsection
