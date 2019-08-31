fd2225686d1030117e0dda6637c58661de130d6f
Gavin Quirk <gavinquirk@gmail.com>
2019-08-31 15:57:16 -0700

    auth and db connection

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    PIX
                 </div>
 
                 <div class="links">
