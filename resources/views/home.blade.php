f776ca2589c4125117c3f2dbff8278a6a972b593
Gavin Quirk <gavinquirk@gmail.com>
2019-08-31 17:02:39 -0700

    images as placeholders

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 <div class="container">
+
   <div class="row">
     <div class="col-3">
       <img src="https://www.rawshorts.com/freeicons/wp-content/uploads/2017/01/media-pict-camera.png" class="rounded-circle"
@@ -22,6 +23,21 @@
       </div>
       <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati distinctio ipsum placeat tempora consequatur deleniti blanditiis voluptates natus pariatur veritatis.</div>
       <div><a href="#"></a>www.pix.org</div>
+    </div>
+  </div>
+
+  <div class="row pt-5">
+    <div class="col-4">
+      <img src="https://scontent-lax3-1.cdninstagram.com/vp/8665050ec554619dcf77f80a37a46109/5E130990/t51.2885-15/sh0.08/e35/s640x640/67980021_2202052933427189_6407359992029428126_n.jpg?_nc_ht=scontent-lax3-1.cdninstagram.com" alt="Computer"
+      class="w-100">
+    </div>
+    <div class="col-4">
+      <img src="https://scontent-lax3-1.cdninstagram.com/vp/8d1f625e9d714d44e6a7d2a697442717/5E144A2D/t51.2885-15/sh0.08/e35/s640x640/68657064_406038826685251_8840140737341399690_n.jpg?_nc_ht=scontent-lax3-1.cdninstagram.com" alt="Computer"
+      class="w-100">
+    </div>
+    <div class="col-4">
+      <img src="https://scontent-lax3-1.cdninstagram.com/vp/e13788e2e26e2b337b7ab86e6cadd4e6/5DFACF81/t51.2885-15/sh0.08/e35/p640x640/67524515_156209612234747_3747271840880064739_n.jpg?_nc_ht=scontent-lax3-1.cdninstagram.com" alt="Computer"
+      class="w-100">
     </div>
   </div>
 </div>
