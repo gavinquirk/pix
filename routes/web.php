e1075900c3dae86c36387c4ed520136dca3dc324
Gavin Quirk <gavinquirk@gmail.com>
2019-09-02 13:22:50 -0700

    Post index for showing followed users posts

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,18 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
 Route::post('follow/{user}', 'FollowsController@store');
 
+// Post Routes
+Route::get('/', 'PostsController@index');
 Route::get('/p/create', 'PostsController@create');
 Route::post('/p', 'PostsController@store');
 Route::get("/p/{post}", 'PostsController@show');
 
+// Profile Routes
 Route::get('/profile/{user}', 'ProfilesController@index')->name('profile.show');
 Route::get('/profile/{user}/edit', 'ProfilesController@edit')->name('profile.edit');
 Route::patch('/profile/{user}', 'ProfilesController@update')->name('profile.update');
