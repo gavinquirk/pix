7998aaac2f098e58cdcb9f6a841e4815b1073657
Gavin Quirk <gavinquirk@gmail.com>
2019-09-02 13:47:32 -0700

    caching for post count, followers and following

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+
 use Intervention\Image\Facades\Image;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use App\User;
 
 class ProfilesController extends Controller
@@ -11,7 +13,20 @@ class ProfilesController extends Controller
   public function index(User $user)
   {
     $follows = (auth()->user()) ? auth()->user()->following->contains($user->id) : false;
-    return view('profiles/index', compact('user', 'follows'));
+
+    $postCount = Cache::remember('count.posts.' . $user->id, now()->addSeconds(30), function () use ($user) {
+      return $user->posts->count();
+    });
+
+    $followersCount = Cache::remember('count.followers.' . $user->id, now()->addSeconds(30), function () use ($user) {
+      return $user->profile->followers->count();
+    });
+
+    $followingCount = Cache::remember('count.following.' . $user->id, now()->addSeconds(30), function () use ($user) {
+      return $user->following->count();
+    });
+
+    return view('profiles/index', compact('user', 'follows', 'postCount', 'followersCount', 'followingCount'));
   }
 
   public function edit(User $user) {
